c30a70dc07fed9b7c17549991698d98741d11cd8
James Aduke <adukework@gmail.com>
2019-07-18 10:23:08 +0300

    Create the recycler view adapter

---
+++ app/src/main/java/com/example/notes/NoteRecyclerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.notes;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>{
+    private final Context mContext;
+    private final LayoutInflater mLayoutInflater;
+
+    public NoteRecyclerAdapter(Context mContext) {
+        this.mContext = mContext;
+        mLayoutInflater = LayoutInflater.from(mContext);
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public  class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
