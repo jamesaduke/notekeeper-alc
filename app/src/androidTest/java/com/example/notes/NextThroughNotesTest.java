fc35854cad8ce1060db4101dec746f80f7d24159
James Aduke <adukework@gmail.com>
2019-07-30 11:35:56 +0300

    Testing on pause

---
+++ app/src/androidTest/java/com/example/notes/NextThroughNotesTest.java
@@ -0,0 +1,22 @@
+//package com.example.notes;
+//
+//import static org.junit.Assert.*;
+//import org.junit.Rule;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
+//import android.support.test.rule.ActivityTestRule;
+////import android.support.test.runner.AndroidJUnit4;
+//import static android.support.test.espresso.Espresso.onView;
+//import static android.support.test.espresso.action.ViewActions.*;
+//import static android.support.test.espresso.matcher.ViewMatchers.*;
+//import static android.support.test.espresso.assertion.ViewAssertions.*;
+//import static org.hamcrest.Matchers.*;
+//import java.util.List;
+//
+//
+//@RunWith(AndroidJUnit4.class)
+//
+//public class NextThroughNotesTest {
+//    public ActivityTestRule<MainActivity>
+//}
